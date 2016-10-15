b4722d5efc18057cd32590512b6048e231ffc2a0
Mat Munn <mat@matmunn.me>
2016-10-16 08:41:22 +1100

    Tiny bit more on pricing page
    Signed-off-by: Mat Munn <mat@matmunn.me>

--- resources/views/home/pricing.blade.php
+++ resources/views/home/pricing.blade.php
@@ -19,6 +19,9 @@
                     <p class="dployr-grey font-serif">
                         While we work on dployr we'd love for you to trial it, break it, and tell us what's wrong with it so we can make it better for everyone.
                     </p>
+                    <p class="dployr-grey font-serif">
+                        Send us an email at support (at) dployr.io and we'll do what we can for you.
+                    </p>
                 </div>
             </div>
         </div>

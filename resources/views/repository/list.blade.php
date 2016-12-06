05d76239aa9a23dffd61b39a51f68ec46319697a
Mat Munn <mat@matmunn.me>
2016-12-06 18:22:14 +1100

    Fixed JS
    Signed-off-by: Mat Munn <mat@matmunn.me>

--- resources/views/repository/list.blade.php
+++ resources/views/repository/list.blade.php
@@ -116,5 +116,16 @@
 @endsection
 
 @section('scripts')
-    <script src="/js/repository/list.js"></script>
+<script type="text/javascript">
+$(document).ready(function () {
+    $('.new-repository-button').on('click', function () {
+        swal({
+            title: "New repository",
+            html: "Choose your repository type:<br /><br /><a class='btn btn-color-normal' href='{{ action('RepositoryController@new') }}'>Self Hosted</a> <a class='btn btn-color-normal disabled' href='#' title='Coming Soon!'>Github</a><br /><br /><a class='btn btn-color-normal disabled' href='#' title='Coming Soon!'>Bitbucket</a>",
+            showCancelButton: true ,
+            showConfirmButton: false
+        });
+    });
+});
+</script>
 @endsection

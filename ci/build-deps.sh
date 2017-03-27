98e7bccfdb071ce84aa5c493c2b94be677386dcf
Mat Munn <mat@matmunn.me>
2017-03-28 09:59:23 +1100

    update deps
    Signed-off-by: Mat Munn <mat@matmunn.me>

--- ci/build-deps.sh
+++ ci/build-deps.sh
@@ -1,6 +1,8 @@
 apt-get update -yqq
-apt-get install git wget -yqq
-docker-php-ext-install gd
+apt-get install -yqq git wget libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng12-dev
+docker-php-ext-install -j$(nproc) iconv mcrypt
+docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+docker-php-ext-install -j$(nproc) gd
 wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"

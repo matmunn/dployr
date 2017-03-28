4715053181f7e01b1e7d3be2e4abc554c7bb3263
Mat Munn <mat@matmunn.me>
2017-03-28 13:30:34 +1100

    Updated deps
    Signed-off-by: Mat Munn <mat@matmunn.me>

--- ci/build-deps.sh
+++ ci/build-deps.sh
@@ -1,12 +1,10 @@
 apt-get update -yqq
-apt-get install -yqq git wget unzip libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng12-dev
-docker-php-ext-install -j$(nproc) iconv mcrypt
+apt-get install -yqq git wget libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev
 docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
-docker-php-ext-install -j$(nproc) gd
+docker-php-ext-install mbstring mcrypt pdo_mysql curl json intl gd xml zip bz2 opcache
 wget https://composer.github.io/installer.sig -O - -q | tr -d '\n' > installer.sig
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === file_get_contents('installer.sig')) { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php'); unlink('installer.sig');"
 php composer.phar install
-
